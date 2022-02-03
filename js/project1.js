e8168997bee5d7f296bf3f079cb741d1ea4c3eba
amirkh75 <amirp.1375@gmail.com>
2022-02-03 14:40:56 -0500

    s4 and start phase2.

--- js/project1.js
+++ js/project1.js
@@ -1,34 +1,106 @@
+//  js and jquery just exec when full page load.
+$(function() { 
 
-// var a=5;
+    // for loading animate
+    $("#loading").fadeTo(2000,0.8).fadeTo(1000,0).fadeOut(500,0);
 
-// document.getElementsByClassName("span-navbar")[0].style.color = "red";
+    // slow scroling
+    $("a").on('click', function(e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            var hash = this.hash;
+            $('html , body').animate({
+                scrollTop: $(hash).offset().top -30
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    $(".sections_").mouseover(function() {
+        $("#sections").css({"border": "palegreen 6px solid"});
+    })
+
+    $(".sections_").mouseout(function() {
+        $("#sections").css({"border": "rosybrown 5px solid"});
+    })
+
+});
+    // $("li").slideUp("5000");
+
+    // $(".droplink").click(function (e) { 
+    //     e.preventDefault();
+    //     $("li").slideDown("slow");
+        
+    // });
+
+    
+
+    // $("#about").click(function (e) { 
+    //     e.preventDefault();
+    //     $("#navbar").slideToggle("slow");
+        
+    // });
 
 
-// if (a==5)
-//     console.log("equal")
+    // $("#section1").click(
+    //     function() {
+    //         $("#sections").css({"border": "palegreen 3px solid"});
+    //     }
+    // );
+
+    // $("#section2").click(
+    //     function() {
+    //         alert("hi babe!");
+    //     }
+    // );
+
+    // var a=5;
+
+    // document.getElementsByClassName("span-navbar")[0].style.color = "red";
+
+
+    // if (a==5)
+    //     console.log("equal")
 
 
 
 
 
-// console.log(a);
+    // console.log(a);
 
-// var a = [0,1,2,3]
-// console.log(a)
-// function change_textp() {
-//     document.getElementById("about").innerHTML = "hi!";
-// }
+    // var a = [0,1,2,3]
+    // console.log(a)
+    // function change_textp() {
+    //     document.getElementById("about").innerHTML = "hi!";
+    // }
 
-var flagp = 0;
+//     var flagp = 0;
 
-function flag_func() {
+//     function flag_func() {
 
-    if (flagp == 0){
-        document.getElementById("section1").style.visibility = "hidden";
-        flagp = 1;
-    }
-    else {
-        document.getElementById("section1").style.visibility = "visible";
-        flagp = 0;
-    }
-}
+//         if (flagp == 0){
+//             document.getElementById("section1").style.visibility = "hidden";
+//             flagp = 1;
+//         }
+//         else {
+//             document.getElementById("section1").style.visibility = "visible";
+//             flagp = 0;
+//         }
+//     }
+
+// });
+
+    // jquery
+
+    // $("li").hide();
+    // $("div").hide();
+
+    // this is equal to next code
+    // $( document ).ready(function() {
+    //     console.log("ready!");
+    // });
+
+    // $(function() {
+    //     console.log("ready");
+    // });
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+$("a").on('click', function(e) {
+        if (this.hash !== "") {
+            e.preventDefault();
+            var hash = this.hash;
+            $('html , body').animate({
+                scrollTop: $(hash).offset().top -30
+            }, 800, function(){
+                 window.location.hash = hash;
+            });
+        }
+    });
